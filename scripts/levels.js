ebee0ea2162a18a9a778d88f93b32ea7752db471
ad1352 <dwelly.412@gmail.com>
2024-10-21 13:05:50 +0100

    Added level description

--- scripts/levels.js
+++ scripts/levels.js
@@ -1,1 +1,13 @@
+/*
+    -Levels should cover the full screen
+    - Consist of 32x32px tiles
+    - Levels will be in a 16:9 ratio
+    - Leave an extra 32 px at the top for GUI
+    - 1 = enemy spawns
+    - 2 = track for enemies
+    - 3 = weak points
+    - 4 = placeable turret space
+    - MORE WILL BE ADDED
+*/
+
 let levels = [];
