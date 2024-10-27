473af0afabe59365d03782de67651d236e17d06b
ad1352 <dwelly.412@gmail.com>
2024-10-27 13:31:14 +0000

    Added tile object

--- scripts/moonbaseObjects.js
+++ scripts/moonbaseObjects.js
@@ -1,7 +1,8 @@
-class BaseObject
+class BaseObject extends Phaser.GameObjects.Sprite
 {
-    constructor()
+    constructor(scene, texture, xPos, yPos)
     {
+        super(scene, xPos, yPos, texture)
         this.boardX = -1;
         this.boardY = -1;
         this.health = 100;
@@ -15,7 +16,25 @@ class BaseObject
     }
 }
 
-class BaseTurret extends BaseObject
+class Tile extends Phaser.GameObjects.Sprite
+{
+    constructor(scene, tileX, tileY, texture, forPlayer)
+    {
+        super(scene, tileX, tileY, texture)
+        this.occupant = null;
+        this.isTurretSpace = forPlayer;
+    }
+}
+
+class BasicTurret extends BaseObject
+{
+    constructor(scene, xPos, yPos)
+    {
+        super(scene, "basicTurret", xPos, yPos);
+    }
+}
+
+class BasicEnemy extends BaseObject
 {
     constructor()
     {
@@ -23,18 +42,10 @@ class BaseTurret extends BaseObject
     }
 }
 
-class BaseEnemy extends BaseObject
-{
-    constructor()
-    {
-
-    }
-}
-
-class BaseProjectile extends BaseObject
+class BasicProjectile extends BaseObject
 {
     constructor(originX, originY)
     {
-
+        
     }
 }
