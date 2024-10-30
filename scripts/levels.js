b580823ec1851bdb8029feef985ddb8d9362ab36
ad1352 <dwelly.412@gmail.com>
2024-10-30 12:09:40 +0000

    Added data for first level

--- scripts/levels.js
+++ scripts/levels.js
@@ -4,10 +4,39 @@
     - Levels will be in a 16:9 ratio
     - Leave an extra 32 px at the top for GUI
     - 1 = enemy spawns
-    - 2 = track for enemies
-    - 3 = weak points
-    - 4 = placeable turret space
+    - 2 - 13 track for enemies
+    - 0 = placeable turret space
     - MORE WILL BE ADDED
+
+
+    COMPASS HEADINGS:
+    straight tiles:
+    2 - NORTH
+    3 - EAST
+    4 - SOUTH
+    5 - WEST
+
+    curves:
+    6 - SOUTH TO WEST
+    7 - WEST TO NORTH
+    8 - NORTH TO EAST
+    9 - EAST TO SOUTH
+    10 - WEST TO SOUTH
+    11 - NORTH TO WEST
+    12 - EAST TO NORTH
+    13 - SOUTH TO EAST
 */
 
-let LEVELS = [];
+const LEVELS = [
+    [
+        [0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,4,0,0,0,0,0,0,0,13,3,3,3,9,0,],
+        [0,8,9,0,0,0,0,0,0,2,0,0,0,4,0,0],
+        [0,0,4,0,0,0,0,0,0,2,0,0,0,4,0,0],
+        [0,0,8,3,3,9,0,0,0,2,0,0,0,4,0,0],
+        [0,0,0,0,0,4,0,0,0,2,0,0,0,4,0,0],
+        [0,0,0,0,0,8,3,3,3,12,0,0,0,4,0,],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,4,0,0]
+    ]
+];
