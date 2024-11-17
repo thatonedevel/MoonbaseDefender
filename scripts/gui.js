f0b0c8ed5e107d72ddd56c6fad1668698885bdb9
ad1352 <dwelly.412@gmail.com>
2024-11-17 17:49:02 +0000

    Added calls to BuildableGhost::enable

--- scripts/gui.js
+++ scripts/gui.js
@@ -1,19 +1,28 @@
+const { Game } = require("phaser");
+
 function createBasicTurret()
 {
-
+    buildableGhost.enable("basicTurret")
 }
 
 function createSolarPanel()
 {
-
+    buildableGhost.enable("solarPanel");
 }
 
 function createShieldGenerator()
 {
-
+    buildableGhost.enable("shieldGenerator");
 }
 
 function togglePause()
 {
-    
+    if (currentGameState == GameStates.PAUSED)
+    {
+        currentGameState = GameStates.PLAYING;
+    }
+    else if (currentGameState == GameStates.PLAYING)
+    {
+        currentGameState = GameStates.PAUSED;
+    }
 }
